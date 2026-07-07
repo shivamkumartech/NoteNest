4bfd63043605869066e909dccde6048e80e384e5
Shivam Kumar <shivanshkumarchauhan@gmail.com>
2026-07-07 20:27:28 +0530

    feat: update Axios base URL to use dynamic environment variable for production

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const BACKEND_URL = axios.create({
-    baseURL: "http://localhost:4001/api/v1/notesapp/"
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 export default BACKEND_URL
