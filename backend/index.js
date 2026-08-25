c32ac49a6c0886ef9390283061200edf540db338
Shivam Kumar <shivanshkumarchauhan@gmail.com>
2026-08-26 00:35:50 +0530

    feat: initialize backend server with MongoDB connection and core API middleware

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
 import noteRoutes from "./routes/note.route.js";
 import authRoutes from "./routes/auth.route.js";
-import cookieParser from "cookie-parser";
 
 dotenv.config();
+
+// Connect to Database
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// DB connection
-try {
-  mongoose.connect(process.env.MONGO_URL);
-  console.log("connected");
-} catch (error) {
-  console.log("error", error);
-}
-
-// routing middleware
-
+// Core Middlewares
 app.use(express.json());
-
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
   }),
 );
-
 app.use(cookieParser());
 
-// Note routes
+// API Routes
 app.use("/api/v1/notesapp", noteRoutes);
-
-// Auth routes
 app.use("/api/v1/notenest/auth", authRoutes);
 
 app.listen(port, () => {
